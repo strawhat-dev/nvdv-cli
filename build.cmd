a0578cec36777e43e4145244834f292c2664e540
strawhat-dev <63446377+strawhat-dev@users.noreply.github.com>
2025-02-13 21:41:57 -0800

    update msvc build script

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-cd "%~dp0" && rd /s /q out && md out
+cd "%~dp0" && rm -rf out && mkdir out
 
 if not exist nvapi\amd64\ git.exe submodule update --init --remote
 
 call vcvars64.bat && cl.exe /std:c++latest /MD /O2 /W4 ^
   /EHsc /I nvapi main.cpp user32.lib shell32.lib ^
   /link /LIBPATH:nvapi\amd64 /OUT:out\nvdv.exe ^
-  && del /f main.obj
+  && rm -f main.obj
